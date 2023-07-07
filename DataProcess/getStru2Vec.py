7ada0d2e30513a52eac62542628ceb01f2382ea6
Aut0matas <43371529+Aut0matas@users.noreply.github.com>
2023-07-08 03:28:33 +0800

    move paths to standalone file

--- DataProcess/getStru2Vec.py
+++ DataProcess/getStru2Vec.py
@@ -13,39 +13,6 @@ from DataProcess.sqlang_structured import (
 )
 
 sys.path.append("..")
-PYTHON_PATHS = {
-    "PYTHON_WORD": "data_processing/hnn_process/data/word_dict/python_word_vocab_dict.txt",
-    "WV": "data_processing/hnn_process/embeddings/python/python_word_vocab_final.pkl",
-    "WORD": "data_processing/hnn_process/embeddings/python/python_word_dict_final.pkl",
-    "STAQC_NEW": "data_processing/hnn_process/ulabel_data/staqc/python_staqc_unlabeled_data.txt",
-    "STAQC_DICT": "data_processing/hnn_process/ulabel_data/staqc/python_word_dict.txt",
-    "LARGE_NEW": "data_processing/hnn_process/ulabel_data/large_corpus/multiple/python_large_multiple_unlabeled.txt",
-    "LARGE_DICT": "data_processing/hnn_process/ulabel_data/large_corpus/python_word_dict.txt",
-    "WV_SAVE": "data_processing/hnn_process/ulabel_data/python_word_vocab_final.pkl",
-    "WORD_SAVE": "data_processing/hnn_process/ulabel_data/python_word_dict.txt",
-    "STAQC_F": "data_processing/hnn_process/ulabel_data/staqc/seri_python_staqc_unlabeled_data.pkl",
-    "LARGE_F": "data_processing/hnn_process/ulabel_data/large_corpus/multiple/seri_python_large_multiple_unlabeled.pkl",
-}
-SQL_PATHS = {
-    "SQL_WORD": "data_processing/hnn_process/data/word_dict/sql_word_vocab_dict.txt",
-    "WV": "data_processing/hnn_process/embeddings/sql/sql_word_vocab_final.pkl",
-    "WORD": "data_processing/hnn_process/embeddings/sql/sql_word_dict_final.pkl",
-    "STAQC_NEW": "data_processing/hnn_process/ulabel_data/staqc/sql_staqc_unlabeled_data.txt",
-    "STAQC_DICT": "data_processing/hnn_process/ulabel_data/staqc/sql_word_dict.txt",
-    "LARGE_NEW": "data_processing/hnn_process/ulabel_data/large_corpus/multiple/sql_large_multiple_unlabeled.txt",
-    "LARGE_DICT": "data_processing/hnn_process/ulabel_data/large_corpus/sql_word_dict.txt",
-    "WV_SAVE": "data_processing/hnn_process/ulabel_data/sql_word_vocab_final.pkl",
-    "WORD_SAVE": "data_processing/hnn_process/ulabel_data/sql_word_dict_final.pkl",
-    "STAQC_F": "data_processing/hnn_process/ulabel_data/staqc/seri_sql_staqc_unlabeled_data.pkl",
-    "LARGE_F": "data_processing/hnn_process/ulabel_data/large_corpus/multiple/seri_ql_large_multiple_unlabeled.pkl",
-}
-PATHS = {
-    "PS": "data_processing/hnn_process/embeddings/10_10/python_struc2vec1/data/python_struc2vec.txt",
-    "PS_BIN": "data_processing/hnn_process/embeddings/10_10/python_struc2vec.bin",
-    "SQL": "data_processing/hnn_process/embeddings/10_8_embeddings/sql_struc2vec.txt",
-    "SQL_BIN": "data_processing/hnn_process/embeddings/10_8_embeddings/sql_struc2vec.bin",
-    "LARGE_SINGLE": "data_processing/hnn_process/ulabel_data/large_corpus/single/sql_large_single_label.txt",
-}
 
 
 class MultiPro:
