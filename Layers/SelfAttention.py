9cfa703cc393c78a6221664465efd51f5f547ed3
Aut0matas <43371529+Aut0matas@users.noreply.github.com>
2023-07-08 02:45:22 +0800

    refine other layers

--- Layers/SelfAttention.py
+++ Layers/SelfAttention.py
@@ -1,68 +1,51 @@
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import *
+import keras.backend as backend
+from keras.layers import Layer
 import tensorflow as tf
-import numpy as np
-import os
-import numpy as np
-import random
-import tensorflow as tf
-seed = 42
-np.random.seed(seed)
-tf.random.set_seed(seed)
-os.environ['PYTHONHASHSEED'] = str(seed)
-random.seed(seed)
 
-class selfattention(Layer):
-    def __init__(self,r,da,name,**kwargs):
-        self.r = r
-        self.da = da
+
+class SelfAttention(Layer):
+    def __init__(self, num_heads, hidden_size, name, **kwargs):
+        self.num_heads = num_heads
+        self.hidden_size = hidden_size
         self.scope = name
-        super(selfattention, self).__init__(**kwargs)
+        super(SelfAttention, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        #(120,150)
-        self.Ws1 = self.add_weight(
-            name=f'Ws1{self.scope}',
-            shape=(input_shape[2], self.da),
-            initializer='glorot_uniform',
+        self.weight_1 = self.add_weight(
+            name=f"weight_1_{self.scope}",
+            shape=(input_shape[2], self.hidden_size),
+            initializer="glorot_uniform",
             trainable=True,
         )
-        #(3,120)
-        self.Ws2 = self.add_weight(
-            name=f'Ws2{self.scope}',
-            shape=(self.da, self.r),
-            initializer='glorot_uniform',
+        self.weight_2 = self.add_weight(
+            name=f"weight_2_{self.scope}",
+            shape=(self.hidden_size, self.num_heads),
+            initializer="glorot_uniform",
             trainable=True,
         )
+
     def call(self, inputs, **kwargs):
+        weight_1_output = backend.dot(inputs, self.weight_1)
+        weight_1_output = tf.tanh(tf.transpose(weight_1_output))
+        weight_1_output = tf.transpose(weight_1_output)
+        weight_2_output = backend.softmax(backend.dot(weight_1_output, self.weight_2))
+        attention_score = backend.permute_dimensions(weight_2_output, (0, 2, 1))
+        weighted_input_vectors = tf.matmul(attention_score, inputs)
+        identity_matrix = tf.tile(tf.eye(self.num_heads), [tf.shape(inputs)[0], 1])
+        identity_matrix = tf.reshape(
+            identity_matrix, [-1, self.num_heads, self.num_heads]
+        )
+        penalty_term = tf.square(
+            tf.norm(
+                tf.matmul(attention_score, weight_2_output) - identity_matrix,
+                axis=[-2, -1],
+                ord="fro",
+            )
+        )
+        return [weighted_input_vectors, penalty_term]
 
-        #inputs:(100,4,150)
-
-        #(?,4,120)
-        A1 = K.dot(inputs,self.Ws1)
-        #(120, 4, ?)
-        A1 = tf.tanh(tf.transpose(A1))
-        #(?,4,120)  da*n
-        A1 = tf.transpose(A1)
-        #(?, 4, 3)
-        A_T = K.softmax(K.dot(A1,self.Ws2))
-        #(?,3,4)
-        A = K.permute_dimensions(A_T,(0,2,1))
-
-        #(100, 3, 150)
-        B = tf.matmul(A,inputs)
-        tile_eye = tf.tile(tf.eye(self.r), [tf.shape(inputs)[0], 1])
-        tile_eye = tf.reshape(tile_eye, [-1, self.r, self.r])
-        #(100, 3, 3)
-        AA_T = tf.matmul(A, A_T) -tile_eye
-
-        #(100,)
-        P = tf.square(tf.norm(AA_T, axis=[-2, -1], ord='fro'))
-
-        return [B,P]
     def compute_output_shape(self, input_shape):
-        return [(input_shape[0],self.da,self.r),(input_shape[0],)]
-
-
-
-
+        return [
+            (input_shape[0], self.hidden_size, self.num_heads),
+            (input_shape[0],),
+        ]
