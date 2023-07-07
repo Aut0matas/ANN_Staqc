6e313956ae729ba715b2c2e63e8498cc2eb36173
Aut0matas <43371529+Aut0matas@users.noreply.github.com>
2023-07-08 02:20:57 +0800

    refine LayerNorm.py

--- Layers/LayerNorm.py
+++ Layers/LayerNorm.py
@@ -1,32 +1,19 @@
-from __future__ import print_function
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import *
+from keras.layers import Layer
 import tensorflow as tf
-from tensorflow import *
-import os
-import numpy as np
-import random
-import tensorflow as tf
-seed = 42
-np.random.seed(seed)
-tf.random.set_seed(seed)
-os.environ['PYTHONHASHSEED'] = str(seed)
-random.seed(seed)
-class LayerNormalization(Layer):
 
+
+class LayerNormalization(Layer):
     def __init__(self, epsilon=1e-8, **kwargs):
         self._epsilon = epsilon
         super(LayerNormalization, self).__init__(**kwargs)
 
     def build(self, input_shape):
         self.beta = self.add_weight(
-            shape=(input_shape[-1],),
-            initializer='zero',
-            name='beta')
+            shape=(input_shape[-1],), initializer="zero", name="beta"
+        )
         self.gamma = self.add_weight(
-            shape=(input_shape[-1],),
-            initializer='one',
-            name='gamma')
+            shape=(input_shape[-1],), initializer="one", name="gamma"
+        )
         super(LayerNormalization, self).build(input_shape)
 
     def call(self, inputs):
